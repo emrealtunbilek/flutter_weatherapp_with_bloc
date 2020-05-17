0c0ae698d5c34e5f8c8f33cadfee057c703ca85f
sohrabonline <63860581+sohrabonline@users.noreply.github.com>
2020-05-17 19:31:46 +0400

    Equatable update
    Equatable update

--- lib/blocs/weather/weather_event.dart
+++ lib/blocs/weather/weather_event.dart
@@ -1,17 +1,30 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-@immutable
 abstract class WeatherEvent extends Equatable {
-  WeatherEvent([List props = const []]) : super(props);
+  const WeatherEvent();
 }
 
-class FetchWeatherEvent extends WeatherEvent {
-  final String sehirAdi;
-  FetchWeatherEvent({@required this.sehirAdi}) : super([sehirAdi]);
-}
 
-class RefreshWeatherEvent extends WeatherEvent {
-  final String sehirAdi;
-  RefreshWeatherEvent({@required this.sehirAdi}) : super([sehirAdi]);
+class FetchWeatherEvent extends WeatherEvent{
+  String sehirAdi;
+
+
+  FetchWeatherEvent({@required this.sehirAdi});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [sehirAdi];
+
+}
+class RefreshWeatherEvent extends WeatherEvent{
+  String sehirAdi;
+
+
+  RefreshWeatherEvent({@required this.sehirAdi});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [sehirAdi];
+
 }
